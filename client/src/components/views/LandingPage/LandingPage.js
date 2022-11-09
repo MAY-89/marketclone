08c80205bfb33d5d35af93043d65f8a0ece9b5f4
MAY-89 <hello.git.galic12@naver.com>
2022-11-09 22:29:29 +0900

    메인 페이지 생성

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,53 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Col, Card, Row, Icon } from "antd";
+import Meta from "antd/lib/card/Meta";
 
 function LandingPage() {
+  const [Product, setProduct] = useState([]);
+
+  useEffect(() => {
+    // let body
+    axios.post("/api/product/products").then((response) => {
+      if (response.data.success) {
+        console.log(response.data);
+        setProduct(response.data.productInfo);
+      } else {
+        alert("상품을 가져오는데 실패 했습니다.");
+      }
+    });
+  }, []);
+
+  const renderCards = Product.map((product, index) => {
+    console.log(product)
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+    <Col lg={6} md={8} xs={24} key={index}>
+      <Card cover={<img style={{width: '100%', maxHeight: '150px'}} src={`http://localhost:5000/${product.images[0]}`} />}
+      >
+        <Meta title={product.title} description={`$${product.price}`} />
+      </Card>
+     </Col>
     )
+  });
+
+  return (
+    <div style={{ width: "75%", margin: "3rem auto" }}>
+      <div style={{ textAlign: "center" }}>
+        <h2>
+          Let's Travle Anywere <Icon type="rocket" />
+        </h2>
+      </div>
+
+{/* gutter는 여백을 주는데 [] */}
+      <Row gutter={[16, 16]}>
+        {renderCards}
+        </Row>
+
+      <div style={{ justifyContent: "center" }}>
+        <button>더보기</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
