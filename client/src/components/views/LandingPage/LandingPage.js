26e3b1b5f3eed2b493ffc80604ff2e7c0e0104c0
MAY-89 <hello.git.galic12@naver.com>
2022-11-17 00:17:36 +0900

    이미지 슬라이더 & 이미지 더보기 버튼 활성화

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,33 +1,61 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Col, Card, Row, Icon } from "antd";
+import { Col, Card, Row, Icon, Carousel } from "antd";
 import Meta from "antd/lib/card/Meta";
+import ImageSlider from "../../utils/ImageSlider";
 
 function LandingPage() {
   const [Product, setProduct] = useState([]);
+  const [Skip, setSkip] = useState(0);
+  const [Limit, setLimit] = useState(4);
+  const [PostSize, setPostSize] = useState(0);
 
   useEffect(() => {
     // let body
-    axios.post("/api/product/products").then((response) => {
+
+    let body = {
+      skip: Skip,
+      limit: Limit,
+    };
+    getProduct(body);
+  }, []);
+
+  const getProduct = (body) => {
+    axios.post("/api/product/products", body).then((response) => {
       if (response.data.success) {
         console.log(response.data);
-        setProduct(response.data.productInfo);
+        if (body.loadMore == true) {
+          setProduct([...Product, ...response.data.productInfo]);
+        } else {
+          setProduct(response.data.productInfo);
+        }
+        setPostSize(response.data.postSize);
       } else {
         alert("상품을 가져오는데 실패 했습니다.");
       }
     });
-  }, []);
+  };
+
+  const loadMoreHandler = () => {
+    let skip = Skip + Limit;
+    let body = {
+      skip: skip,
+      limit: Limit,
+      loadMore: true,
+    };
+    getProduct(body);
+    setSkip(skip);
+  };
 
   const renderCards = Product.map((product, index) => {
-    console.log(product)
+    console.log(product);
     return (
-    <Col lg={6} md={8} xs={24} key={index}>
-      <Card cover={<img style={{width: '100%', maxHeight: '150px'}} src={`http://localhost:5000/${product.images[0]}`} />}
-      >
-        <Meta title={product.title} description={`$${product.price}`} />
-      </Card>
-     </Col>
-    )
+      <Col lg={6} md={8} xs={24} key={index}>
+        <Card cover={<ImageSlider images={product.images} />}>
+          <Meta title={product.title} description={`$${product.price}`} />
+        </Card>
+      </Col>
+    );
   });
 
   return (
@@ -38,14 +66,15 @@ function LandingPage() {
         </h2>
       </div>
 
-{/* gutter는 여백을 주는데 [] */}
-      <Row gutter={[16, 16]}>
-        {renderCards}
-        </Row>
+      {/* gutter는 여백을 주는데 [] */}
+      <Row gutter={[16, 16]}>{renderCards}</Row>
 
-      <div style={{ justifyContent: "center" }}>
-        <button>더보기</button>
-      </div>
+      <br />
+      {PostSize >= Limit && (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button onClick={loadMoreHandler}>더보기</button>
+        </div>
+      )}
     </div>
   );
 }
--- server/routes/product.js
+++ server/routes/product.js
@@ -45,13 +45,18 @@ router.post("/", (req, res) => {
 
 router.post("/products", (req, res) => {
 
+  let limit = req.body.limit ? parseInt(req.body.limit) : 20;
+  let skip = req.body.skip ? parseInt(req.body.skip) : 0;
+
   // DB에 있는 모든 Product 가져오기
   // populate 누가 등록 했는지 확인 하기
   Product.find()
   .populate("writer")
+  .skip(skip)
+  .limit(limit)
   .exec((err, productInfo) =>{
     if(err) return res.status(400).json({success : false, err})
-    return res.status(200).json({success: true, productInfo});
+    return res.status(200).json({success: true, productInfo, postSize: productInfo.length});
   })
 
 });
