ed9dd4ad85ac9a0bafcb4c06ce0d9c04e1dccdf5
MAY-89 <hello.git.galic12@naver.com>
2022-12-01 00:23:47 +0900

    Images Component 태그 반영

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -1,28 +1,47 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ProductImage from "./sections/ProductImage";
+import ProductInfo from "./sections/ProductInfo";
+import { Row, Col } from "antd";
 
 function DetailProductPage(props) {
+  const productId = props.match.params.productId;
 
-    const productId = props.match.params.productId;
+  const [Product, setProduct] = useState({});
 
-    useEffect(() => {
-      axios.get(`/api/product/products_by_id?id=${productId}&type=single`)
-      .then(response => {
-        if(response.data.success){
-            console.log(response.data);
-        }else{
-
+  useEffect(() => {
+    axios
+      .get(`/api/product/products_by_id?id=${productId}&type=single`)
+      .then((response) => {
+        if (response.data.success) {
+          console.log(response.data);
+          setProduct(response.data.product[0]);
+        } else {
         }
-      })
-    }, [])
-    
-
-
-
+      });
+  }, []);
 
   return (
-    <div>DetailProductPage</div>
-  )
+    <div style={{ width: "100%", padding: "3rem 4rem" }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1>{Product.title}</h1>
+      </div>
+      <br />
+
+      <Row gutter={[16, 16]}>
+        <Col lg={12} sm={24}>
+          {/* ProductImage */}
+
+          <ProductImage detail={Product} />
+        </Col>
+
+        <Col lg={12} sm={24}>
+          {/* Product Info */}
+          <ProductInfo></ProductInfo>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default DetailProductPage
+export default DetailProductPage;
