b97c41c0757c1ed8131434b9f5cfc1fa2cbc98bb
MAY-89 <hello.git.galic12@naver.com>
2022-12-04 23:07:17 +0900

    카트 추가 기능 개발

--- client/src/components/views/DetailProductPage/sections/ProductInfo.js
+++ client/src/components/views/DetailProductPage/sections/ProductInfo.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Button, Descriptions } from "antd";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../../../_actions/user_actions";
 
-function ProductInfo() {
+function ProductInfo(props) {
+  const dispatch = useDispatch();
 
+  const clickHandler = () => {
+    // 필요한 정보를 Cart 필드에 넣어주겠음
+    /**
+     * 1. 상품 ID
+     * 2. 상품 갯수
+     * 3. 장바구니 추가 일자
+     */
+    // 여기서는 axios에 바로 넣지 않겠음 리덕스 훅 이용
+    dispatch(addToCart(props.detail._id));
     
+  };
   return (
-    <div>ProductInfo</div>
-  )
+    <div>
+      <Descriptions title="Product Info" bordered>
+        <Descriptions.Item label="Price">
+          {props.detail.price}
+        </Descriptions.Item>
+        <Descriptions.Item label="Sold">{props.detail.sold}</Descriptions.Item>
+        <Descriptions.Item label="View">{props.detail.views}</Descriptions.Item>
+        <Descriptions.Item label="Description">
+          {props.detail.description}
+        </Descriptions.Item>
+      </Descriptions>
+
+      <br />
+      <br />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Button size="large" shape="round" type="danger" onClick={clickHandler}>
+          Add to Cart
+        </Button>
+      </div>
+    </div>
+  );
 }
 
-export default ProductInfo
+export default ProductInfo;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { User } = require("../models/User");
 
@@ -9,63 +9,113 @@ const { auth } = require("../middleware/auth");
 //=================================
 
 router.get("/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAdmin: req.user.role === 0 ? false : true,
-        isAuth: true,
-        email: req.user.email,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        role: req.user.role,
-        image: req.user.image,
-    });
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
 });
 
 router.post("/register", (req, res) => {
+  const user = new User(req.body);
 
-    const user = new User(req.body);
-
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
-        });
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
     });
+  });
 });
 
 router.post("/login", (req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user)
-            return res.json({
-                loginSuccess: false,
-                message: "Auth failed, email not found"
-            });
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
 
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
 
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("w_authExp", user.tokenExp);
-                res
-                    .cookie("w_auth", user.token)
-                    .status(200)
-                    .json({
-                        loginSuccess: true, userId: user._id
-                    });
-            });
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("w_authExp", user.tokenExp);
+        res.cookie("w_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
         });
+      });
     });
+  });
 });
 
 router.get("/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true
-        });
-    });
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "", tokenExp: "" },
+    (err, doc) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true,
+      });
+    }
+  );
+});
+
+router.post("/addToCart", auth, (req, res) => {
+  // req.user 는 auth에 있는 user 정보를 바로 사용할수 있음
+  User.findOne({ _id: req.user._id }),
+  // User Collection에 해당 유저의 정보를 가져옴
+    (err, userInfo) => {
+      // 가져온 정보에서 카트에다 넣으려고 하는 상품이 이미 들어 있는지 확인한다.
+      let duplicate = false;
+      userInfo.cart.forEach((item) => {
+        if (item.id == req.body.productId) {
+          duplicate = true;
+        }
+      });
+    };
+  if (duplicate == true) {
+    // 상품이 있을때 카운트 추가
+    User.findOneAndUpdate(
+        {_id:req.user._id, "cart.id":req.body.productId},
+        { $inc : {"cart.$.quantity" : 1}}, //increment : 증가
+        { new : true }, // 업데이트 후에 결과 값을 받기 위해서 true, 반대는 false
+        (err, userInfo) => {
+            if(err) return res.status(200).json({success: false, err})
+
+            return res.status(200).send(userInfo.cart);
+
+        }
+    )   
+  } else {
+    // 상품이 없을때 새로이 추가
+    User.findOneAndUpdate(
+        {_id:req.user._id},
+        {
+            $push : {
+                cart:{
+                    id: req.body.productId,
+                    quantity:1,
+                    data: Date.now()
+                }
+            }
+        },
+        {new : true},
+        (err, userInfo) =>{
+            if(err) return res.status(200).json({success: false, err})
+
+            return res.status(200).send(userInfo.cart);
+        }
+    )
+  }
 });
 
 module.exports = router;
